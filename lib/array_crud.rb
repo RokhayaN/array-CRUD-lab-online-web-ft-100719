8b7af31469c44b888a9f68130942c8af631cfaf3
RokhayaN <rokhayamuslima1@gmail.com>
2019-10-11 01:26:17 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,24 +1,35 @@
-def create_an_empty_array
-  
+ 
+ def
+ create_an_empty_array [ ]
 end
 
-def create_an_array
-  
+def
+create_an_array=[1,2,3,4]
 end
 
-def add_element_to_end_of_array(array, element)
-  
+def
+ add_element_to_end_of_array = [1,2,3,4]
+  add_element_to_end_of_array << 5
+  print add_element_to_end_of_array
 end
 
-def add_element_to_start_of_array(array, element)
-  
+def 
+  add_element_to_start_of_array = ["b","c","d"]
+  add_element_to_start_of_array.unshift("a")
+  print add_element_to_start_of_array
 end
 
-def remove_element_from_end_of_array(array)
+def 
+  remove_element_from_end_of_array = [1,2,3,4,5]
+  remove_element_from_end_of_array.pop(5)
+  print remove_element_from_end_of_array
+end 
   
-end
 
-def remove_element_from_start_of_array(array)
+def 
+  remove_element_from_start_of_array = [1,2,3,4]
+  remove_element_from_start_of_array.shift[1]
+  print remove_element_from_start_of_array
   
 end
 
